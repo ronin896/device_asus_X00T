8e7ae180ac7d5a5278885248c7f772cb086ec434
RONIN808 <71311970+RONIN808@users.noreply.github.com>
2021-07-29 12:20:23 +0530

    Update nad_X00T.mk

--- nad_X00T.mk
+++ nad_X00T.mk
@@ -22,6 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common Nusantara stuff
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
+# Product spec
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+scr_resolution := 1080
+
+# Offline Charger
+USE_PIXEL_CHARGING := true
+
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
